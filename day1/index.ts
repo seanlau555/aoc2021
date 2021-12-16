af29c1a05caaf39ede89e5b1fdbb790748bc1115
seanlau555 <lthon0225@gmail.com>
2021-12-16 17:44:37 +0800

    add day1

--- day1/index.ts
+++ day1/index.ts
@@ -1,11 +1,52 @@
 import * as fs from 'fs'
 
-let input = fs.readFileSync('./input.txt').toString()
+let numbers = fs
+  .readFileSync('./input.txt')
+  .toString()
+  .split('\n')
+  .map(line => parseInt(line))
 
-const last = (arr: Array<number>) => {
-  return arr[arr.length - 1]
+let previous: number | undefined
+let count = 0
+
+numbers.forEach(num => {
+  if (previous && num > previous) {
+    count++
+  }
+  previous = num
+})
+
+console.log('star one: ', count)
+
+///
+type Triplet = [number, number, number]
+let triplets: Triplet[] = []
+
+numbers.forEach((num, i) => {
+  let a = num,
+    b = numbers[i + 1],
+    c = numbers[i + 2]
+  if (a && b && c) {
+    triplets.push([a, b, c])
+  }
+})
+
+let previousTriplet: Triplet | undefined
+let tripletCount = 0
+
+const sum = (triplet: Triplet): number => {
+  return triplet.reduce((acc, item) => acc + item, 0)
 }
 
-const l = last([1, 2, 3])
+const tripletIncreased = (newer: Triplet, older: Triplet): boolean => {
+  return sum(newer) > sum(older)
+}
 
-console.log(l)
+triplets.forEach(triplet => {
+  if (previousTriplet && tripletIncreased(triplet, previousTriplet)) {
+    tripletCount++
+  }
+  previousTriplet = triplet
+})
+
+console.log('star two: ', tripletCount)
